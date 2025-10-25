bff7a5e7c6480dd5e0ecb119df763618190c7910
drinkel <lu.li.ad.sm@gmail.com>
2025-10-25 13:57:20 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,7 +8,21 @@ def words(str):
             if text[i] != ' ':
                 continue
             else:
-                words.append(text[j:i])
+                w.append(text[j:i])
                 j = i + 1
         except IndexError:
-            words.append(text[j:])
+            w.append(text[j:])
+    
+    return w
+
+
+#Отсортировать массив чисел по возрастанию
+#Sort an array of numbers in ascending order
+def sorting(arr):
+    temparr, length = arr, len(arr)
+    for i in range(1, length):
+        for j in range(i):
+            while i < length and temparr[i] < temparr[j]:
+                temparr[j], temparr[i] = temparr[i], temparr[j]
+                
+    return temparr
