61acd186021e5ea982734e5afe70bdd654098fa3
drinkel <lu.li.ad.sm@gmail.com>
2025-10-25 14:38:27 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -26,3 +26,28 @@ def sorting(arr):
                 temparr[j], temparr[i] = temparr[i], temparr[j]
                 
     return temparr
+
+
+#Сортировка чисел от меньшего к большему и наоборот
+#
+def sorting(arr, from_to):
+    #from_to must be "min-max" or "max-min"
+    sortednums, nums, length = [], arr[:], len(arr)
+    
+    while len(sortednums) != length:
+        j = 0
+        
+        for i in range(len(nums)):
+            if from_to == "min-max":
+                if nums[j] > nums[i]:
+                    j = i
+            elif from_to == "max-min":
+                if nums[j] < nums[i]:
+                    j = i
+            else:
+                return False
+            
+        sortednums.append(nums[j])
+        nums.pop(j)
+        
+    return sortednums
